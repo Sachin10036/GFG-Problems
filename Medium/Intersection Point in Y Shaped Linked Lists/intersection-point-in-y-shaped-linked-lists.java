7c35ebad0fd614a763dda36a38fdfeaa9e34e8cb
Sachin Suthar <127216860+Sachin10036@users.noreply.github.com>
2024-06-07 13:19:51 +0530

    Updated Solution - GfG to GitHub

--- Medium/Intersection Point in Y Shaped Linked Lists/intersection-point-in-y-shaped-linked-lists.java
+++ Medium/Intersection Point in Y Shaped Linked Lists/intersection-point-in-y-shaped-linked-lists.java
@@ -1,6 +1,6 @@
 //{ Driver Code Starts
 import java.util.*;
-
+import java.io.*;
 
 class Node
 {
@@ -35,76 +35,83 @@ public void addToTheLast(Node node)
 }
 
   /* Function to print linked list */
-    void printList()
+    void printList(PrintWriter out)
     {
         Node temp = head;
         while (temp != null)
         {
-           System.out.print(temp.data+" ");
+           out.print(temp.data+" ");
            temp = temp.next;
         }  
-        System.out.println();
+        out.println();
     }
 	
 	 
  
      /* Driver program to test above functions */
-    public static void main(String args[])
+    public static void main(String args[])throws IOException
     {
        
          
         /* Constructed Linked List is 1->2->3->4->5->6->
            7->8->8->9->null */
-         Scanner sc = new Scanner(System.in);
-		 int t=sc.nextInt();
+         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+         PrintWriter out=new PrintWriter(System.out);
+		 int t=Integer.parseInt(in.readLine().trim());
 		 
 		 while(t>0)
          {
-			int n1 = sc.nextInt();
-			int n2 = sc.nextInt();
-			int n3 = sc.nextInt();
+            String s[]=in.readLine().trim().split(" ");
+			int n1 = Integer.parseInt(s[0]);
+			int n2 = Integer.parseInt(s[1]);
+			int n3 = Integer.parseInt(s[2]);
 			LinkedList_Intersection llist1 = new LinkedList_Intersection();
 		    LinkedList_Intersection llist2 = new LinkedList_Intersection();
 			LinkedList_Intersection llist3 = new LinkedList_Intersection();
 			
-				int a1=sc.nextInt();
+			    s=in.readLine().trim().split(" ");
+				int a1=Integer.parseInt(s[0]);
 				Node head1= new Node(a1);
 				Node tail1= head1;
 				
 				for (int i = 1; i < n1; i++) 
 				{
-					int a = sc.nextInt(); 
+					int a = Integer.parseInt(s[i]); 
 					tail1.next = (new Node(a));
 					tail1= tail1.next;
 				}
 			
-			
-				int b1=sc.nextInt();
+			    s=in.readLine().trim().split(" ");
+				int b1=Integer.parseInt(s[0]);
 				Node head2 = new Node(b1);
 				Node tail2 = head2;
 				for (int i = 1; i < n2; i++) 
 				{
-					int b = sc.nextInt();  
+					int b = Integer.parseInt(s[i]);  
 					tail2.next = (new Node(b));
 					tail2= tail2.next;
 				}
-				if(n3>0){
-				int c1=sc.nextInt();
-				Node head3= new Node(c1);
-				tail1.next = head3;
-				tail2.next = head3;
-				Node tail3=head3;
-				for (int i = 1; i < n3; i++) 
-				{
-					int c = sc.nextInt();   
-					tail3.next = (new Node(c));
-					tail3= tail3.next;
+				if(n3!=0){
+				    s=in.readLine().trim().split(" ");
+    				int c1=Integer.parseInt(s[0]);
+    				Node head3= new Node(c1);
+    				tail1.next = head3;
+    				tail2.next = head3;
+    				Node tail3=head3;
+    				for (int i = 1; i < n3; i++) 
+    				{
+    					int c = Integer.parseInt(s[i]); 
+    					tail3.next = (new Node(c));
+    					tail3= tail3.next;
+    				}
 				}
-				}
+				
+				
 				Intersect obj = new Intersect();
-				System.out.println(obj.intersectPoint(head1, head2));
+				out.println(obj.intersectPoint(head1, head2));
 			t--;			
          }
+         out.close();
     }
 }
 
@@ -129,20 +136,17 @@ class Intersect
 	int intersectPoint(Node head1, Node head2)
 	{
          // code here
-         if(head1 == null || head2 == null) return -1;
+         if(head1 == null || head2 == null){
+             return 0;
+         }
          Node t1 = head1;
          Node t2 = head2;
          while(t1 != t2){
+             t1 = t1.next;
+             t2 = t2.next;
+             if(t1 == t2) return t1.data;
              if(t1 == null) t1 = head2;
-             else{
-                 t1 = t1.next;
-             }
              if(t2 == null) t2 = head1;
-             else{
-                 t2 = t2.next;
-             }
-             if(t1 == null && t2 == null) return -1;
-             
          }
          return t1.data;
 	}
