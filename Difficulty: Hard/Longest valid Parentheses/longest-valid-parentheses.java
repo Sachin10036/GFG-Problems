56394305df7e8867d88d5c68909eb2bc25b30aad
Sachin Suthar <127216860+Sachin10036@users.noreply.github.com>
2024-09-16 23:29:03 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String S = in.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.maxLength(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int maxLength(String S){
+        // code here
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        int maxl = 0;
+        for(int i=0; i<S.length(); i++){
+            if(S.charAt(i) == '('){
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(st.isEmpty()){
+                    st.push(i);
+                }
+                maxl = Math.max(maxl, i - st.peek());
+            }
+        }
+        return maxl;
+    }
+}
