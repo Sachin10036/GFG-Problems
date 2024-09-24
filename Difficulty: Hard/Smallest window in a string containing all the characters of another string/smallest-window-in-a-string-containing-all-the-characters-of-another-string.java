0cac755d5a0e37d1ce10684104dbe29e5f88a8e0
Sachin Suthar <127216860+Sachin10036@users.noreply.github.com>
2024-09-24 23:28:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window in a string containing all the characters of another string/smallest-window-in-a-string-containing-all-the-characters-of-another-string.java
+++ Difficulty: Hard/Smallest window in a string containing all the characters of another string/smallest-window-in-a-string-containing-all-the-characters-of-another-string.java
@@ -20,42 +20,36 @@ class GFG {
 // } Driver Code Ends
 
 
-class Solution
-{
-    //Function to find the smallest window in the string s consisting
-    //of all the characters of string p.
-    public static String smallestWindow(String s, String p)
-    {
-        // Your code here
-        int l=0;
-        int r=0;
-        HashMap<Character,Integer> mpp=new HashMap<>();
-        int cnt=0;
-        int sindex=-1;
-        int minlen=Integer.MAX_VALUE;
-        String st="";
-        for(int i=0;i<p.length();i++){
-            mpp.put(p.charAt(i), mpp.getOrDefault(p.charAt(i), 0) + 1);
-        }
-        while(r<s.length()){
-            char ch=s.charAt(r);
-            if(mpp.containsKey(ch) && mpp.get(ch)>0){   
-                cnt++;
+class Solution {
+    public static String smallestWindow(String s, String p) {
+        if (s.length() < p.length()) return "-1";
+
+        Map<Character, Integer> pMap = new HashMap<>();
+        for (char ch : p.toCharArray()) pMap.put(ch, pMap.getOrDefault(ch, 0) + 1);
+
+        int left = 0, minLength = Integer.MAX_VALUE, minStart = 0, count = 0;
+        Map<Character, Integer> windowMap = new HashMap<>();
+
+        for (int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+            if (pMap.containsKey(rightChar)) {
+                windowMap.put(rightChar, windowMap.getOrDefault(rightChar, 0) + 1);
+                if (windowMap.get(rightChar) <= pMap.get(rightChar)) count++;
             }
-            mpp.put(ch,mpp.getOrDefault(ch,0)-1);            
-            while(cnt==p.length()){
-                if(r-l+1<minlen){
-                    minlen=r-l+1;
-                    sindex=l;
+
+            while (count == p.length()) {
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    minStart = left;
                 }
-                mpp.put(s.charAt(l),mpp.get(s.charAt(l))+1);
-                if(mpp.get(s.charAt(l))>0){
-                    cnt--;
+                char leftChar = s.charAt(left);
+                if (pMap.containsKey(leftChar)) {
+                    windowMap.put(leftChar, windowMap.get(leftChar) - 1);
+                    if (windowMap.get(leftChar) < pMap.get(leftChar)) count--;
                 }
-                l++;
-                }
-            r++;
+                left++;
+            }
         }
-        return sindex==-1?"-1":s.substring(sindex,sindex+minlen);
+        return minLength == Integer.MAX_VALUE ? "-1" : s.substring(minStart, minStart + minLength);
     }
 }
