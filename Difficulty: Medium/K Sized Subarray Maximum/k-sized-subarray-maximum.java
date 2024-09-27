569a63b1ad6e1683a14fbef4ee6dc2313f59a955
Sachin Suthar <127216860+Sachin10036@users.noreply.github.com>
2024-09-27 22:02:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -1,46 +1,37 @@
 //{ Driver Code Starts
-//Initial template for JAVA
+// Initial template for JAVA
 
-import java.util.*;
 import java.io.*;
 import java.lang.*;
+import java.util.*;
 
-class Main
-{
-    public static void main(String args[])
-    {
-        //taking input using class Scanner
-        Scanner sc = new Scanner(System.in);
-        
-        //taking total count of testcases
-        int t = sc.nextInt();
-        
-        
-        
-        while(t-- > 0)
-        {
-            //taking total number of elements
-            int n = sc.nextInt();
-            
-            //taking size of subArrays 
-            int k = sc.nextInt();
-            
-            //Declaring and Intializing an array of size n
-            int arr[] = new int[n];
-            
-            //adding all the elements to the array 
-            for(int i = 0; i <n; i++)
-            {
-                arr[i] =sc.nextInt();
+public class Main {
+    public static void main(String args[]) throws IOException {
+        // taking input using class Scanner
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            // taking total number of elements
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
             }
-            
-            //Calling the method max_of_subarrays of class solve
-            //and storing the result in an ArrayList
-            ArrayList <Integer> res = new Solution().max_of_subarrays(arr, n, k);
-            
-            //printing the elements of the ArrayList
-            for (int i = 0; i < res.size(); i++)
-                System.out.print (res.get (i) + " ");
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            ArrayList<Integer> res = new Solution().max_of_subarrays(k, arr);
+
+            // printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
             System.out.println();
         }
     }
@@ -48,29 +39,37 @@ class Main
 // } Driver Code Ends
 
 
-//User function template for JAVA
+// User function template for JAVA
 
-class Solution
-{
-    //Function to find maximum of each subarray of size k.
-    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
-    {
+class Solution {
+    // Function to find maximum of each subarray of size k.
+    public ArrayList<Integer> max_of_subarrays(int k, int arr[]) {
         // Your code here
-        ArrayList<Integer> ans = new ArrayList<>();
-        Deque<Integer> deque = new LinkedList<>();
+         ArrayList<Integer> list = new ArrayList<Integer>();
         
-        for(int i=0; i<n; i++){
-            if(!deque.isEmpty() && deque.peek() == i-k){
-                deque.poll();
+        int tempMax= findMax(0,k-1,arr);
+        list.add(tempMax);
+        
+        for(int i=1; i<=arr.length-k; i++){
+            if(tempMax == arr[i-1] ){
+                tempMax = findMax(i, i+k-1,arr);
+                list.add(tempMax);
             }
-            while(!deque.isEmpty() && arr[deque.peekLast()] < arr[i]){
-                deque.pollLast();
-            }
-            deque.offer(i);
-            if(i >= k-1){
-                ans.add(arr[deque.peek()]);
+            else{
+                    tempMax = Math.max(tempMax, arr[i+k-1]);
+                    list.add(tempMax);
             }
         }
-        return ans;
+        return list;
+        
+    }
+    
+    public int findMax(int start, int end, int arr[]){
+        int max=0;
+        while(start <= end){
+            max = Math.max(max, arr[start]);
+            start++;
+        }
+        return max;
     }
 }
