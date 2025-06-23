cc0a05cd0fb3f0f57a153129035cb518800edd58
Sachin Suthar <127216860+Sachin10036@users.noreply.github.com>
2025-06-23 22:09:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum sum/minimum-sum.java
+++ Difficulty: Medium/Minimum sum/minimum-sum.java
@@ -1,78 +1,37 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String arr[] = br.readLine().split(" ");
-            int a[] = new int[arr.length];
-
-            for (int i = 0; i < arr.length; i++) {
-                a[i] = Integer.parseInt(arr[i]);
-            }
-            Solution obj = new Solution();
-            int f = 0;
-            String A = obj.minSum(a);
-            System.out.println(A);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     String minSum(int[] arr) {
         // code here
-         int[] count = new int[10];
-       
-        for (int x : arr) count[x]++;
+        int n = arr.length;
         
-        StringBuilder num1 = new StringBuilder();
-        StringBuilder num2 = new StringBuilder();
-        boolean toggle = true;
-       
-        for (int i = 1; i < 10; i++) {
-            while (count[i] > 0) {
-                if (toggle) {
-                    num1.append(i);
-                } else {
-                    num2.append(i);
-                }
-                toggle = !toggle;
-                count[i]--;
-            }
-        }
-       
-        StringBuilder sumResult = new StringBuilder();
-        int i = num1.length() - 1;
-        int j = num2.length() - 1;
-        int carry = 0;
-
-        while (i >= 0 || j >= 0 || carry > 0) {
-            int digit1 = (i >= 0) ? num1.charAt(i--) - '0' : 0;
-            int digit2 = (j >= 0) ? num2.charAt(j--) - '0' : 0;
-            int sum = digit1 + digit2 + carry;
-            sumResult.append(sum % 10);
-            carry = sum / 10;
-        }
-
-        sumResult.reverse();
-
-        while (sumResult.length() > 1 && sumResult.charAt(0) == '0') {
-            sumResult.deleteCharAt(0);
+        Arrays.sort(arr);
+        
+        int start = 0;
+        while(start < n && arr[start] == 0) {
+            start++;
         }
         
-        return sumResult.toString();
-         
+        StringBuilder res = new StringBuilder();
+        
+        int i=n-1;
+        int sum, carry = 0;
+        while(i>start) {
+            sum = arr[i] + arr[i-1] + carry;
+            res.append(sum % 10);
+            
+            carry = sum/10;
+            
+            i-=2;
+        }
+        if(i == start) {
+            sum = arr[start] + carry;
+            res.append(sum%10);
+            carry = sum/10;
+        }
+        if(carry > 0) {
+            res.append(carry);
+        }
+        
+        res.reverse();
+        return res.toString();
     }
 }
